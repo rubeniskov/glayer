95ddcc7e175adf34e0469fe01bebacc7c9fdca95
Rubén López Gómez <loslopez1@icloud.com>
2016-10-09 20:22:04 +0200

    add: 2d vertex shader

--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,9 +1,9 @@
-attribute vec4 vert;
-varying vec2 v_coord0;
-varying vec2 v_coord1;
-
+attribute vec4 aPosition;
+attribute vec2 aQuadPosition;
+varying vec2 vCoord;
+varying vec2 vQuadCoord;
 void main() {
-    gl_Position = vec4(vert.xy, 0.0, 1.0);
-    v_coord0 = vert.zw;
-    v_coord1 = vert.zw;
+    gl_Position = vec4(aPosition.xy, 0.0, 1.0);
+    vCoord = aPosition.zw;
+    vQuadCoord = aQuadPosition.xy;
 }

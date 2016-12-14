9df668604936d6d7fdc0957333a75c394ae9b01b
Rubén López Gómez <loslopez1@icloud.com>
2016-12-14 01:56:01 +0100

    add: sampler class

--- src/sampler.js
+++ src/sampler.js
@@ -1,5 +1,16 @@
-export default class Sampler {
-    constructor(options) {
+import glTexture from "gl-texture2d";
+import pool from "typedarray-pool";
+import ndarray from "ndarray";
 
+export default class Sample {
+    constructor(context, shape, dtype) {
+        this._texture = glTexture(context, ndarray(pool.malloc(shape[0] * shape[1] * [2], dtype || 'uint8'), shape));
+    }
+    set(data) {
+        this._texture.setPixels(data);
+        return this;
+    }
+    dispose() {
+        this._texture.dispose();
     }
 }
